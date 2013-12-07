b37feaa67112b53175c82088b2a434dc6413a231
Fergonesh <pashashenhofer@gmail.com>
2013-12-07 23:33:20 +0200

    added db table;

--- dbscript/db2311.sql
+++ dbscript/db2311.sql
@@ -1,21 +1,31 @@
 USE SocNet;
-CREATE TABlE USERS
-(
-  id_user int not NULL AUTO_INCREMENT ,
-  lastname varchar (255) NOT NULL,
-  firstname varchar (255) NOT NULL,
-  email varchar(255) NOT NULL,
-  password varchar (255) NOT NULL,
-  PRIMARY KEY (id_user)
-);
-
-CREATE TABlE Message
+CREATE TABlE MESSAGE
 (
   id_mess int not NULL AUTO_INCREMENT ,
-  Title varchar (255) NOT NULL,
+  title varchar (255) NOT NULL,
   bodymess varchar (255) NOT NULL,
   create_date varchar(255) NOT NULL,
   id_user int not NULL,
   PRIMARY KEY (id_mess),
   FOREIGN KEY (id_user) REFERENCES USERS(id_user)
+);
+
+CREATE TABlE GROUPS
+(
+  id_group int not NULL AUTO_INCREMENT ,
+  group_name varchar (255) NOT NULL,
+  desctiption varchar (255) NOT NULL,
+  id_user int not NULL,
+  PRIMARY KEY (id_group),
+  FOREIGN KEY (id_user) REFERENCES USERS(id_user)
+);
+
+CREATE TABlE GROUP_PARTICIPANT
+(
+  id_gp int not NULL AUTO_INCREMENT ,
+  id_user int not NULL,
+  id_group int not NULL,
+  PRIMARY KEY (id_gp),
+  FOREIGN KEY (id_user) REFERENCES USERS(id_user),
+  FOREIGN KEY (id_group) REFERENCES GROUPS(id_group)
 );
