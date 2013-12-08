1b3835eb8294bb4a1762c830185033fe9a0c678b
Fergonesh <pashashenhofer@gmail.com>
2013-12-08 20:18:17 +0200

    added methods to groups dao;

--- src/SocNET/DAO/GroupDAO.java
+++ src/SocNET/DAO/GroupDAO.java
@@ -7,6 +7,8 @@ import SocNET.publicDAO.GroupDAOInterface;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -53,4 +55,102 @@ public class GroupDAO extends AbstractDBConn implements GroupDAOInterface {
                 }
             });
         }
+
+    @Override
+    public boolean addParticipant(final int userID,final  int groupID) throws MyException {
+        return booleanOperation(new AbstractDBConn.WrapperDBOperation<Boolean>() {
+            @Override
+            public Boolean doMethod(Connection dataBase) throws SQLException, MyException {
+                PreparedStatement prep = dataBase.prepareStatement(
+                        "INSERT INTO GROUPS_PARTICIPANT (id_user, id_group) values(?,?)"
+                );
+                prep.setInt(1,userID);
+                prep.setInt(2,groupID);
+                //String str = prep.;
+
+                prep.executeUpdate();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public List<Groups> getAllGroups() throws MyException {
+        return booleanOperation(new WrapperDBOperation<List<Groups>>() {
+
+            @Override
+            public List<Groups> doMethod(Connection dataBase) throws MyException, SQLException {
+                PreparedStatement prep = dataBase.prepareStatement(
+                        "SELECT * FROM GROUPS"
+                );
+
+                java.sql.ResultSet res = prep.executeQuery();
+                List<Groups> glist = new ArrayList<Groups>();
+                while(res.next()){
+                    int id = res.getInt(1);
+                    String name = res.getString(2);
+                    String desc = res.getString(3);
+                    int idOwner = res.getInt(4);
+                    glist.add(new Groups(id,name,desc,idOwner));
+                }
+
+                return glist;
+
+            }
+        });
+    }
+
+    @Override
+    public List<Groups> getMyGroups(final int userID) throws MyException {
+        return booleanOperation(new WrapperDBOperation<List<Groups>>() {
+
+            @Override
+            public List<Groups> doMethod(Connection dataBase) throws MyException, SQLException {
+                PreparedStatement prep = dataBase.prepareStatement(
+                        "SELECT * FROM GROUPS WHERE GROUPS.id_user=?"
+                );
+                prep.setInt(1,userID);
+                java.sql.ResultSet res = prep.executeQuery();
+                List<Groups> glist = new ArrayList<Groups>();
+                while(res.next()){
+                    int id = res.getInt(1);
+                    String name = res.getString(2);
+                    String desc = res.getString(3);
+                    int idOwner = res.getInt(4);
+                    glist.add(new Groups(id,name,desc,idOwner));
+                }
+
+                return glist;
+
+            }
+        });
+    }
+
+    @Override
+    public List<Groups> getMySubsGroups(final int userID) throws MyException {
+        return booleanOperation(new WrapperDBOperation<List<Groups>>() {
+
+            @Override
+            public List<Groups> doMethod(Connection dataBase) throws MyException, SQLException {
+                PreparedStatement prep = dataBase.prepareStatement(
+                        "SELECT GROUPS.id_group,GROUPS.group_name,GROUPS.descrtiption,GROUPS.id_user " +
+                                " FROM GROUPS, GROUP_PARTICIPANT WHERE GROUP_PARTICIPANT.id_user=? " +
+                                " AND GROUPS.id_group=GROUP_PARTICIPANT.id_group;"
+                );
+                prep.setInt(1,userID);
+                java.sql.ResultSet res = prep.executeQuery();
+                List<Groups> glist = new ArrayList<Groups>();
+                while(res.next()){
+                    int id = res.getInt(1);
+                    String name = res.getString(2);
+                    String desc = res.getString(3);
+                    int idOwner = res.getInt(4);
+                    glist.add(new Groups(id,name,desc,idOwner));
+                }
+
+                return glist;
+
+            }
+        });
+    }
 }
--- src/SocNET/publicDAO/GroupDAOInterface.java
+++ src/SocNET/publicDAO/GroupDAOInterface.java
@@ -4,6 +4,8 @@ import SocNET.model.Groups;
 import SocNET.model.User;
 import SocNET.myexception.MyException;
 
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
  * User: pasha
@@ -14,4 +16,14 @@ import SocNET.myexception.MyException;
 public interface GroupDAOInterface {
     public boolean insert(Groups gr) throws MyException;
     public boolean delete(int userID) throws MyException;
+    public List<Groups> getAllGroups() throws  MyException;
+    public List<Groups> getMyGroups(int userID) throws MyException; //user is creator
+    public List<Groups> getMySubsGroups(int userID) throws MyException;  //user is member
+
+    //for group_participant
+
+    public boolean addParticipant(int userID,int groupID) throws MyException;
+
+
+
 }
